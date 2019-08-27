bcc0cead099a37150b480d0c7c04a05750609a9a
Alexandar Petrov <Alexandar Petrov@APETROV>
2019-08-27 15:24:13 +0300

    Add '02.StackSum' project

--- StacksAndQueues/02.StackSum/Program.cs
+++ StacksAndQueues/02.StackSum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.StackSum
 {
@@ -6,7 +8,50 @@ namespace _02.StackSum
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+			var numbers = new Stack<int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				numbers.Push(int.Parse(input[i].ToString()));
+			}
+
+			while (true)
+			{
+				string[] commandInput = Console.ReadLine().Split(" ");
+				string command = commandInput[0].ToLower();
+
+				if (command == "end")
+				{
+					break;
+				}
+				else if (command == "add")
+				{
+					for (int i = 1; i <= 2; i++)
+					{
+						numbers.Push(int.Parse(commandInput[i]));
+					}
+				}
+				else if (command == "remove")
+				{
+					int countRemoves = int.Parse(commandInput[1]);
+
+					if (countRemoves > numbers.Count)
+					{
+						continue;
+					}
+					else
+					{
+						for (int i = 0; i < countRemoves; i++)
+						{
+							numbers.Pop();
+						}
+					}
+				}
+			}
+
+			var sum = numbers.Sum();
+			Console.WriteLine($"Sum: {sum}");
 		}
 	}
 }
