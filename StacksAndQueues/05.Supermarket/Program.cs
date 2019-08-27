0b81066cce7f8654eb513a627f9e2b01917730ac
Alexandar Petrov <Alexandar Petrov@APETROV>
2019-08-27 16:53:52 +0300

    Add '05.Supermarket' project

--- StacksAndQueues/05.Supermarket/Program.cs
+++ StacksAndQueues/05.Supermarket/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05.Supermarket
 {
@@ -6,7 +7,40 @@ namespace _05.Supermarket
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var people = new Queue<string>();
+			int counter = 0;
+			bool findPaid = false;
+
+			while (true)
+			{
+				string name = Console.ReadLine();
+
+				if (name.ToLower() == "paid")
+				{
+					findPaid = true;
+
+					foreach (var currentName in people)
+					{
+						Console.WriteLine(currentName);
+					}
+
+					counter = 0;
+
+					continue;
+				}
+				else if (name.ToLower() == "end")
+				{
+					break;
+				}
+				else
+				{
+					people.Enqueue(name);
+				}
+
+				counter++;
+			}
+
+			Console.WriteLine($"{counter} people remaining.");
 		}
 	}
 }
