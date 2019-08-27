7039dfd99ef526320541e2f441df0c122dea21f0
Alexandar Petrov <Alexandar Petrov@APETROV>
2019-08-27 15:43:56 +0300

    Add '03.SimpleCalculator' project

--- StacksAndQueues/03.SimpleCalculator/Program.cs
+++ StacksAndQueues/03.SimpleCalculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03.SimpleCalculator
 {
@@ -6,7 +8,26 @@ namespace _03.SimpleCalculator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] input = Console.ReadLine().Split();
+			var symbols = new Stack<string>(input.Reverse());
+			int result = int.Parse(symbols.Pop());
+
+			while (symbols.Any())
+			{
+				string nextSymbol = symbols.Pop();
+
+				if (nextSymbol == "+")
+				{
+					result += int.Parse(symbols.Pop());
+				}
+				else if (nextSymbol == "-")
+				{
+					result -= int.Parse(symbols.Pop());
+
+				}
+			}
+
+			Console.WriteLine(result);
 		}
 	}
 }
