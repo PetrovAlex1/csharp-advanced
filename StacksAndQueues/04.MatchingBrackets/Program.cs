1a55d7ab3f32db0aa511ef1d8e1bd8fc4b37356b
Alexandar Petrov <Alexandar Petrov@APETROV>
2019-08-27 16:08:55 +0300

    Add '04.MatchingBrackets' project

--- StacksAndQueues/04.MatchingBrackets/Program.cs
+++ StacksAndQueues/04.MatchingBrackets/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04.MatchingBrackets
 {
@@ -6,7 +8,21 @@ namespace _04.MatchingBrackets
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = Console.ReadLine();
+			var scopeIndexes = new Stack<int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i] == '(')
+				{
+					scopeIndexes.Push(i);
+				}
+				else if (input[i] == ')')
+				{
+					int startIndex = scopeIndexes.Pop();
+					Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+				}
+			}
 		}
 	}
 }
