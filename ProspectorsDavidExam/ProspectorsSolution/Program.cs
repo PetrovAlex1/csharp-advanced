a80529d97bb0b69dae826106dde80f28ba847eb0
Alexandar Petrov <Alexandar Petrov@APETROV>
2019-10-04 10:34:54 +0300

    Add "ProspectorsDavidExam" solution

---
+++ ProspectorsDavidExam/ProspectorsSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProspectorsSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
