492aaa2d20d1504f295366285dcc768f4d9d8a2f
AlexDays <AlexDays@students.softuni.bg>
2019-10-16 12:08:56 +0300

    Add '04.SymbolInMatrix' project

--- MultidimensionalArrays/04.SymbolInMatrix/Program.cs
+++ MultidimensionalArrays/04.SymbolInMatrix/Program.cs
@@ -6,7 +6,37 @@ namespace _04.SymbolInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int matrixBounds = int.Parse(Console.ReadLine());
+            var matrix = new char[matrixBounds, matrixBounds];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = Console.ReadLine();
+                var currentSymbolsRow = new char[matrix.GetLength(0)];
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    string currentSymbol = currentRow.Substring(col, 1);
+                    currentSymbolsRow[col] = char.Parse(currentSymbol);
+                    matrix[row, col] = char.Parse(currentSymbol);
+                }
+            }
+
+            char symbolToSearch = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == symbolToSearch)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbolToSearch} does not occur in the matrix");
         }
     }
 }
