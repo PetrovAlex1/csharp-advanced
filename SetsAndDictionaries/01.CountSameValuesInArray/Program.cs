a3ca66244bfe1bddb55f5d1b3ae3c2b17919b78c
Alexandar Petrov <Alexandar Petrov@APETROV>
2019-11-06 16:21:44 +0200

    Add "01.CountSameValuesInArray" project

--- SetsAndDictionaries/01.CountSameValuesInArray/Program.cs
+++ SetsAndDictionaries/01.CountSameValuesInArray/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01.CountSameValuesInArray
 {
@@ -6,7 +8,27 @@ namespace _01.CountSameValuesInArray
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var dict = new Dictionary<double, int>();
+			var input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (!dict.ContainsKey(input[i]))
+				{
+					dict[input[i]] = 1;
+					continue;
+				}
+
+				dict[input[i]]++;
+			}
+
+			foreach (var kvp in dict)
+			{
+				double number = kvp.Key;
+				int count = kvp.Value;
+
+				Console.WriteLine($"{number} - {count} times");
+			}
 		}
 	}
 }
