6800286664d6174531fb53c182f629aac3aa6681
AlexDays <AlexDays@students.softuni.bg>
2020-04-05 13:34:50 +0300

    Add "01.SortEvenNumbers" project

--- FunctionalPrograming/01.SortEvenNumbers/Program.cs
+++ FunctionalPrograming/01.SortEvenNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.SortEvenNumbers
 {
@@ -6,7 +7,20 @@ namespace _01.SortEvenNumbers
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = Console.ReadLine()
+				.Split(new[] { ",", " " }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToList();
+
+			Func<int, bool> filterNumbers = n => n % 2 == 0;
+			Func<int, int> orderNumbers = n => n;
+
+			var filteredList = input
+				.Where(filterNumbers)
+				.OrderBy(orderNumbers)
+				.ToList();
+
+			Console.WriteLine(string.Join(", ", filteredList));
 		}
 	}
 }
