326e9024c4c3e0f9b2ade8bd511e47a36b574aee
AlexDays <AlexDays@students.softuni.bg>
2020-04-05 14:29:43 +0300

    Add "03.CountUppercaseWords" project

--- FunctionalPrograming/03.CountUppercaseWords/Program.cs
+++ FunctionalPrograming/03.CountUppercaseWords/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.CountUppercaseWords
 {
@@ -6,7 +7,14 @@ namespace _03.CountUppercaseWords
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+
+			var input = Console.ReadLine()
+				.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+				.Where(checker)
+				.ToList();
+
+			input.ForEach(Console.WriteLine);
 		}
 	}
 }
