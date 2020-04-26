a0664bfe2651574a950b9ca5a35857f2f01b872a
AlexDays <AlexDays@students.softuni.bg>
2020-04-26 15:35:00 +0300

    Add "08.CustomComparator" project

--- FunctionalProgramingExercise/08.CustomComparator/Program.cs
+++ FunctionalProgramingExercise/08.CustomComparator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _08.CustomComparator
 {
@@ -6,7 +8,33 @@ namespace _08.CustomComparator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			Func<int, bool> checkEvenNumber = x => x % 2 == 0;
+			Func<int, bool> checkOddNumber = x => x % 2 != 0;
+			var evenNumbers = new List<int>();
+			var oddNumbers = new List<int>();
+			var resultList = new List<int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (checkEvenNumber(input[i]))
+				{
+					evenNumbers.Add(input[i]);
+				}
+			}
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (checkOddNumber(input[i]))
+				{
+					oddNumbers.Add(input[i]);
+				}
+			}
+
+			evenNumbers.Sort();
+			oddNumbers.Sort();
+			resultList.AddRange(evenNumbers);
+			resultList.AddRange(oddNumbers);
+			Console.WriteLine(string.Join(" ", resultList));
 		}
 	}
 }
