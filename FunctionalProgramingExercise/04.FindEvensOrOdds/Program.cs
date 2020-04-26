757fe6b61b1a7a3be6a049eb675434f32c548829
AlexDays <AlexDays@students.softuni.bg>
2020-04-26 12:54:46 +0300

    Add "04.FindEvensOrOdds" project

--- FunctionalProgramingExercise/04.FindEvensOrOdds/Program.cs
+++ FunctionalProgramingExercise/04.FindEvensOrOdds/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04.FindEvensOrOdds
 {
@@ -6,7 +8,36 @@ namespace _04.FindEvensOrOdds
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			var resultList = new List<int>();
+			var start = input[0];
+			var end = input[1];
+			var keyWord = Console.ReadLine();
+			Predicate<int> findEvenNumbers = x => x % 2 == 0;
+			Predicate<int> findOddNumbers = x => x % 2 != 0;
+
+			if (keyWord.ToLower() == "odd")
+			{
+				for (int i = start; i <= end; i++)
+				{
+					if (findOddNumbers(i))
+					{
+						resultList.Add(i);
+					}
+				}
+			}
+			else if (keyWord.ToLower() == "even")
+			{
+				for (int i = start; i <= end; i++)
+				{
+					if (findEvenNumbers(i))
+					{
+						resultList.Add(i);
+					}
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", resultList));
 		}
 	}
 }
