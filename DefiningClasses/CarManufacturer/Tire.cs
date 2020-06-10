3eaedaafae0f50eca9562fa418ea819029031455
AlexDays <AlexDays@students.softuni.bg>
2020-06-10 20:10:07 +0300

    Add "CarManufacturer" project

---
+++ DefiningClasses/CarManufacturer/Tire.cs
@@ -0,0 +1,39 @@
+﻿
+namespace CarManufacturer
+{
+	public class Tire
+	{
+		public Tire(int year, double presure)
+		{
+			this.Year = year;
+			this.Presure = presure;
+		}
+
+		private int year;
+		private double presure;
+
+		public int Year
+		{
+			get
+			{
+				return this.year;
+			}
+			set
+			{
+				this.year = value;
+			}
+		}
+
+		public double Presure
+		{
+			get
+			{
+				return this.presure;
+			}
+			set
+			{
+				this.presure = value;
+			}
+		}
+	}
+}
