77450d388949c0408b1ee4559c897ea718bec803
AlexDays <AlexDays@students.softuni.bg>
2020-06-10 20:19:49 +0300

    Add "CarManufacturer" project

---
+++ DefiningClasses/CarManufacturer/Car.cs
@@ -0,0 +1,154 @@
+﻿
+using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+	public class Car
+	{
+		public Car()
+		{
+			this.Make = "VW";
+			this.Model = "Golf";
+			this.Year = 2025;
+			this.FuelQuantity = 200;
+			this.FuelConsumption = 10;
+		}
+
+		public Car(string make, string model, int year)
+			: this()
+		{
+			this.Make = make;
+			this.Model = model;
+			this.Year = year;
+		}
+
+		public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+			: this(make, model, year)
+		{
+			this.FuelQuantity = fuelQuantity;
+			this.FuelConsumption = fuelConsumption;
+		}
+
+		public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+			: this(make, model, year, fuelQuantity, fuelConsumption)
+		{
+			this.Engine = engine;
+			this.Tires = tires;
+		}
+
+		private string make;
+		private string model;
+		private int year;
+		private double fuelConsumption;
+		private double fuelQuantity;
+		private Engine engine;
+		private Tire[] tires;
+
+		public string Make
+		{
+			get
+			{
+				return this.make;
+			}
+			set
+			{
+				this.make = value;
+			}
+		}
+		public string Model
+		{
+			get
+			{
+				return this.model;
+			}
+			set
+			{
+				this.model = value;
+			}
+		}
+		public int Year
+		{
+			get
+			{
+				return this.year;
+			}
+			set
+			{
+				this.year = value;
+			}
+		}
+		public double FuelQuantity
+		{
+			get
+			{
+				return this.fuelQuantity;
+			}
+			set
+			{
+				this.fuelQuantity = value;
+			}
+		}
+		public double FuelConsumption
+		{
+			get
+			{
+				return this.fuelConsumption;
+			}
+			set
+			{
+				this.fuelConsumption = value;
+			}
+		}
+
+		public Engine Engine
+		{
+			get
+			{
+				return this.engine;
+			}
+			set
+			{
+				this.engine = value;
+			}
+		}
+
+		public Tire[] Tires
+		{
+			get
+			{
+				return this.tires;
+			}
+			set
+			{
+				this.tires = value;
+			}
+		}
+
+		public void Drive(double distance)
+		{
+			double result = (distance / 100) * fuelConsumption;
+
+			if (fuelQuantity - result > 0)
+			{
+				fuelQuantity -= distance / 100 * fuelConsumption;
+			}
+			else
+			{
+				Console.WriteLine("Not enough fuel to perform this trip!");
+			}
+		}
+
+		public string WhoAmI()
+		{
+			StringBuilder carInfo = new StringBuilder();
+
+			carInfo.AppendLine($"Make: {this.Make}");
+			carInfo.AppendLine($"Model: {this.Model}");
+			carInfo.AppendLine($"Year: {this.Year}");
+			carInfo.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+			return carInfo.ToString();
+		}
+	}
+}
---
+++ DefiningClasses/CarManufacturer/StartUp.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarManufacturer
+{
+	class StartUp
+	{
+		static void Main(string[] args)
+		{
+			var tires = new Dictionary<int, List<Tire>>();
+			int j = 0;
+
+			while (true)
+			{
+				var input = Console.ReadLine().Split();
+				var tireList = new List<Tire>();
+
+				if (input[0] == "No")
+				{
+					break;
+				}
+
+				for (int i = 0; i < input.Length; i += 2)
+				{
+					var year = int.Parse(input[i]);
+					var presure = double.Parse(input[i + 1]);
+					var tire = new Tire(year, presure);
+					tireList.Add(tire);
+				}
+
+				tires.Add(j, tireList);
+				j++;
+			}
+
+			var engines = new List<Engine>();
+
+			while (true)
+			{
+				var input = Console.ReadLine().Split();
+
+				if (input[0] == "Engines")
+				{
+					break;
+				}
+
+				var horsePower = int.Parse(input[0]);
+				var cubicCapacity = double.Parse(input[1]);
+				var engine = new Engine(horsePower, cubicCapacity);
+				engines.Add(engine);
+			}
+
+			var cars = new List<Car>();
+
+			while (true)
+			{
+				var input = Console.ReadLine().Split();
+
+				if (input[0] == "Show")
+				{
+					break;
+				}
+
+				var make = input[0];
+				var model = input[1];
+				var year = int.Parse(input[2]);
+				var fuelQuantity = double.Parse(input[3]);
+				var fuelConsumption = double.Parse(input[4]);
+				var engineIndex = int.Parse(input[5]);
+				var tireIndex = int.Parse(input[6]);
+				var engine = engines[engineIndex];
+				var tire = tires[tireIndex].ToArray();
+				var car = new Car(make, model, year, fuelQuantity, fuelConsumption, engine, tire);
+				cars.Add(car);
+			}
+
+			foreach (var car in cars)
+			{
+				var presureSum = 0.0;
+
+				if (car.Year >= 2017)
+				{
+					if (car.Engine.HorsePower > 330)
+					{
+						for (int i = 0; i < car.Tires.Length; i++)
+						{
+							presureSum += car.Tires[i].Presure;
+						}
+
+						if (presureSum >= 9 && presureSum < 10)
+						{
+							car.Drive(20);
+							PrintInformation(car);
+						}
+					}
+				}
+			}
+		}
+
+		private static void PrintInformation(Car car)
+		{
+			Console.WriteLine($"Make: {car.Make}" +
+							  $"\nModel: {car.Model}" +
+							  $"\nYear: {car.Year} " +
+							  $"\nHorsePowers: {car.Engine.HorsePower}" +
+							  $"\nFuelQuantity: {car.FuelQuantity}");
+		}
+	}
+}
