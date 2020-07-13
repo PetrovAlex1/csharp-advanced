ae121124a06b937ca4570bb9d90055396af73beb
AlexDays <AlexDays@students.softuni.bg>
2020-07-13 15:31:39 +0300

    Add "GenericSwapMethodString" project

---
+++ GenericsExercise/GenericSwapMethosString/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericSwapMethosString
+{
+	public class Box<T>
+	{
+		public List<T> Items { get; set; }
+
+		public List<T> SwapItems(int firstIndex, int secondIndex)
+		{
+			var firstItem = Items[firstIndex];
+			var secondItem = Items[secondIndex];
+
+			Items[firstIndex] = secondItem;
+			Items[secondIndex] = firstItem;
+
+			return Items;
+		}
+
+		public void Print()
+		{
+			foreach (var item in Items)
+			{
+				Console.WriteLine($"{item.GetType()}: {item}");
+			}
+		}
+	}
+}
--- GenericsExercise/GenericSwapMethosString/Program.cs
+++ GenericsExercise/GenericSwapMethosString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericSwapMethosString
 {
@@ -6,7 +7,22 @@ namespace GenericSwapMethosString
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var n = int.Parse(Console.ReadLine());
+			var box = new Box<string>();
+			box.Items = new List<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				var currentInput = Console.ReadLine();
+				box.Items.Add(currentInput);
+			}
+
+			var swapIndexes = Console.ReadLine().Split();
+			var firstIndex = int.Parse(swapIndexes[0]);
+			var secondIndex = int.Parse(swapIndexes[1]);
+			box.SwapItems(firstIndex, secondIndex);
+			box.Print();
+
 		}
 	}
 }
