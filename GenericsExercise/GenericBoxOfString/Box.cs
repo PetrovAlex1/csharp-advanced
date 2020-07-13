4d19806b24e831fea819181320ea3f7862b4419a
AlexDays <AlexDays@students.softuni.bg>
2020-07-13 14:37:35 +0300

    Add "GenericBoxOfString" project

---
+++ GenericsExercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace GenericBoxOfString
+{
+	public class Box<T>
+	{
+		private T data;
+
+		public Box(T item)
+		{
+			this.data = item;
+		}
+	
+		public override string ToString()
+		{
+			return $"{data.GetType()}: {data}";
+		}
+	}
+}
--- GenericsExercise/GenericBoxOfString/Program.cs
+++ GenericsExercise/GenericBoxOfString/Program.cs
@@ -6,7 +6,14 @@ namespace GenericBoxOfString
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var n = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+			{
+				var currentInput = Console.ReadLine();
+				var currentBox = new Box<string>(currentInput);
+				Console.WriteLine(currentBox.ToString());
+			}
 		}
 	}
 }
