f89fe9753565b1698f998b3b8695444374ab0002
AlexDays <AlexDays@students.softuni.bg>
2020-07-14 14:37:58 +0300

    Add "GenericCountMethodString" project

---
+++ GenericsExercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GenericCountMethodString
+{
+	public class Box<T> where T : IComparable
+	{
+		public List<T> items { get; set; }
+
+		public Box()
+		{
+			this.items = new List<T>();
+		}
+
+		public int ComapareElement(T element)
+		{
+			var countBiggerElements = 0;
+
+			foreach (var item in items)
+			{
+				if (item.CompareTo(element) > 0)
+				{
+					countBiggerElements++;
+				}
+			}
+
+			return countBiggerElements;
+		}
+	}
+}
--- GenericsExercise/GenericCountMethodString/Program.cs
+++ GenericsExercise/GenericCountMethodString/Program.cs
@@ -6,7 +6,17 @@ namespace GenericCountMethodString
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var n = int.Parse(Console.ReadLine());
+			var box = new Box<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				var currentInput = Console.ReadLine();
+				box.items.Add(currentInput);
+			}
+
+			var elementToCompare = Console.ReadLine();
+			Console.WriteLine(box.ComapareElement(elementToCompare));
 		}
 	}
 }
