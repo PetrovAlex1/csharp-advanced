306a1862942b550e870d1fc66020e6eebe434707
AlexDays <AlexDays@students.softuni.bg>
2020-07-15 13:26:49 +0300

    Add "GenericCountMethodDouble" project

---
+++ GenericsExercise/GenericCountMethodDouble/Box.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+	public class Box<T> where T : IComparable
+	{
+		public List<T> Items { get; set; }
+
+		public Box()
+		{
+			this.Items = new List<T>();
+		}
+
+		public int CountGreaterElements(T element)
+		{
+			var counter = 0;
+
+			foreach (var item in Items)
+			{
+				if (item.CompareTo(element) > 0)
+				{
+					counter++;
+				}
+			}
+
+			return counter;
+		}
+	}
+}
--- GenericsExercise/GenericCountMethodDouble/Program.cs
+++ GenericsExercise/GenericCountMethodDouble/Program.cs
@@ -6,7 +6,18 @@ namespace GenericCountMethodDouble
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var n = int.Parse(Console.ReadLine());
+			var box = new Box<double>();
+
+			for (int i = 0; i < n; i++)
+			{
+				var currentInput = double.Parse(Console.ReadLine());
+				box.Items.Add(currentInput);
+			}
+
+			var elementToCompare = double.Parse(Console.ReadLine());
+
+			Console.WriteLine(box.CountGreaterElements(elementToCompare));
 		}
 	}
 }
