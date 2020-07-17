eb40e099c471a16025324e4bbf8d3aedb032dbec
AlexDays <AlexDays@students.softuni.bg>
2020-07-17 12:40:10 +0300

    Add "Tuple" project

--- GenericsExercise/Tuple/Program.cs
+++ GenericsExercise/Tuple/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tuple
 {
@@ -6,7 +8,27 @@ namespace Tuple
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var firstIsInt = false;
+			var secondIsDouble = false;
+			var counter = 0;
+			var firstInput = Console.ReadLine().Split();
+			var firstName = firstInput[0];
+			var secondName = firstInput[1];
+			var address = firstInput[2];
+			var fullName = firstName + " " + secondName;
+			var tupleNames = new Tuple<string, string>(fullName, address);
+			Console.WriteLine($"{fullName} -> {address}");
+
+			var secondInput = Console.ReadLine().Split();
+			var name = secondInput[0];
+			var litersOfBeer = double.Parse(secondInput[1]);
+			Console.WriteLine($"{name} -> {litersOfBeer}");
+
+			var thirdInput = Console.ReadLine().Split();
+			var intValue = int.Parse(thirdInput[0]);
+			var doubleValue = double.Parse(thirdInput[1]);
+			var tupleDigits = new Tuple<int, double>(intValue, doubleValue);
+			Console.WriteLine($"{intValue} -> {doubleValue}");
 		}
 	}
 }
---
+++ GenericsExercise/Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Tuple
+{
+	public class Tuple<T>
+	{
+		public T Item1 { get; set; }
+		public T Item2 { get; set; }
+		public Tuple(T item1, T item2)
+		{
+			this.Item1 = item1;
+			this.Item2 = item2;
+		}
+		public override string ToString()
+		{
+			return $"{Item1} -> {Item2}";
+		}
+
+		public void Print()
+		{
+			Console.WriteLine($"{Item1} -> {Item2}");
+		}
+	}
+}
