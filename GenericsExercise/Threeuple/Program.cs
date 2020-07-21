29d25152fe57801b5fca9afa737b72c15b4f4cb4
AlexDays <AlexDays@students.softuni.bg>
2020-07-21 11:28:41 +0300

    Add "Threeuple" project

--- GenericsExercise/Threeuple/Program.cs
+++ GenericsExercise/Threeuple/Program.cs
@@ -6,7 +6,35 @@ namespace Threeuple
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var firstInput = Console.ReadLine().Split();
+			var firstName = firstInput[0];
+			var lastName = firstInput[1];
+			var address = firstInput[2];
+			var town = firstInput[3];
+			var fullName = firstName + " " + lastName;
+			var firstThreeuple = new Threeuple<string, string, string>(fullName, address, town);
+			Console.WriteLine(firstThreeuple.ToString());
+
+			var secondInput = Console.ReadLine().Split();
+			var name = secondInput[0];
+			var litersOfBeer = int.Parse(secondInput[1]);
+			var drunkOrNot = secondInput[2];
+			var isDrunk = false;
+
+			if (drunkOrNot == "drunk")
+			{
+				isDrunk = true;
+			}
+
+			var secondThreeuple = new Threeuple<string, int, bool>(name, litersOfBeer, isDrunk);
+			Console.WriteLine(secondThreeuple.ToString());
+
+			var thirdInput = Console.ReadLine().Split();
+			var accountName = thirdInput[0];
+			var accountBalance = double.Parse(thirdInput[1]);
+			var bankName = thirdInput[2];
+			var thirdThreeuple = new Threeuple<string, double, string>(accountName, accountBalance, bankName);
+			Console.WriteLine(thirdThreeuple.ToString());
 		}
 	}
 }
---
+++ GenericsExercise/Threeuple/Threeuple.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Threeuple
+{
+	public class Threeuple<T, C, V>
+	{
+		public T Item1 { get; set; }
+		public C Item2 { get; set; }
+		public V Item3 { get; set; }
+
+		public Threeuple(T item1, C item2, V item3)
+		{
+			this.Item1 = item1;
+			this.Item2 = item2;
+			this.Item3 = item3;
+		}
+
+		public override string ToString()
+		{
+			return $"{Item1} -> {Item2} -> {Item3}";
+		}
+	}
+}
