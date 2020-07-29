970b0fd73ca271d14bd2cd46aed2a81f2cf8717b
AlexDays <AlexDays@students.softuni.bg>
2020-07-29 13:33:08 +0300

    Add "Library" project

--- IteratorsAndComparators/Library/Program.cs
+++ IteratorsAndComparators/Library/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Library
+namespace IteratorsAndComparators
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var bookOne = new Book("Animal Farm", 2003, "George Orwell");
+			var bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustance");
+			var bookThree = new Book("The Documents in the Case", 1930);
+
+			var libraryOne = new library();
+			var libraryTwo = new library(bookOne, bookTwo, bookThree);
 		}
 	}
 }
---
+++ IteratorsAndComparators/Library/library.cs
@@ -0,0 +1,26 @@
+﻿using IteratorsAndComparators;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+	public class library : IEnumerable<Book>
+	{
+		private List<Book> books;
+
+		public library(params Book[] books)
+		{
+			this.books = new List<Book>(books);
+		}
+
+		public IEnumerator<Book> GetEnumerator()
+		{
+			for (int i = 0; i < books.Count; i++)
+			{
+				yield return books[i];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+	}
+}
