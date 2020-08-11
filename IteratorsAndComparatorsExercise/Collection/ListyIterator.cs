1d13aef81688b15703e83ede2f1cf0ab2ffcf431
AlexDays <AlexDays@students.softuni.bg>
2020-08-11 17:14:24 +0300

    Add "Collection" project

---
+++ IteratorsAndComparatorsExercise/Collection/ListyIterator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+	public class ListyIterator<T> : IEnumerable<T>
+	{
+		private List<T> items;
+		private int index = 0;
+
+		public ListyIterator(params T[] items)
+		{
+			this.items = items.ToList();
+		}
+
+		public bool Move()
+		{
+			var couldMove = false;
+
+			if (index < items.Count)
+			{
+				couldMove = true;
+				this.index++;
+			}
+
+			return couldMove;
+		}
+
+		public bool HasNext()
+		{
+			var hasNext = false;
+
+			if (index < items.Count - 1)
+			{
+				hasNext = true;
+			}
+
+			return hasNext;
+		}
+
+		public void Print()
+		{
+			if (items.Count == 0)
+			{
+				Console.WriteLine("Invalid Operation!");
+			}
+			else
+			{
+				Console.WriteLine(items[index]);
+			}
+		}
+
+		public void PrintAll()
+		{
+			Console.WriteLine(string.Join(" ", items));
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (int i = 0; i < items.Count; i++)
+			{
+				yield return items[i];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+	}
+}
--- IteratorsAndComparatorsExercise/Collection/Program.cs
+++ IteratorsAndComparatorsExercise/Collection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Collection
 {
@@ -6,7 +8,46 @@ namespace Collection
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var listyIterator = new ListyIterator<string>();
+			var command = Console.ReadLine().Split().ToArray();
+
+			while (command[0] != "END")
+			{
+				switch (command[0])
+				{
+					case "Create":
+						if (command.Length == 0)
+						{
+							var list = new List<string>();
+							listyIterator = new ListyIterator<string>(list.ToArray());
+						}
+						else
+						{
+							var list = new List<string>();
+							list.AddRange(command.ToList<string>());
+							list = list.Where(x => x != "Create").ToList();
+							listyIterator = new ListyIterator<string>(list.ToArray());
+						}
+						break;
+					case "Move":
+						Console.WriteLine(listyIterator.Move());
+						break;
+					case "Print":
+						listyIterator.Print();
+						break;
+					case "HasNext":
+						Console.WriteLine(listyIterator.HasNext());
+						break;
+					case "PrintAll":
+						listyIterator.PrintAll();
+						break;
+					default:
+						break;
+				}
+
+				command = Console.ReadLine().Split().ToArray();
+			}
 		}
 	}
 }
+
