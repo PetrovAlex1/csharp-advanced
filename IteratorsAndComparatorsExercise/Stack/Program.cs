9a3612b816a57fb44bf1c58295ff3f343b03c754
AlexDays <AlexDays@students.softuni.bg>
2020-08-14 10:56:03 +0300

    Add "Stack" project

--- IteratorsAndComparatorsExercise/Stack/Program.cs
+++ IteratorsAndComparatorsExercise/Stack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Stack
 {
@@ -6,7 +7,29 @@ namespace Stack
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string[] cymbols = { " ", ", " };
+			var command = Console.ReadLine().Split(cymbols, StringSplitOptions.RemoveEmptyEntries).ToArray();
+			var stack = new Stack<string>();
+
+			while (command[0] != "END")
+			{
+				switch (command[0])
+				{
+					case "Push":
+						var items = command.Where(x => x != "Push").ToList();
+						stack.Push(items);
+						break;
+					case "Pop":
+						stack.Pop();
+						break;
+					default:
+						break;
+				}
+
+				command = Console.ReadLine().Split(cymbols, StringSplitOptions.RemoveEmptyEntries).ToArray();
+			}
+
+			stack.Print();
 		}
 	}
 }
---
+++ IteratorsAndComparatorsExercise/Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+	public class Stack<T> : IEnumerable<T>
+	{
+		private List<T> data = new List<T>();
+
+		public void Push(List<T> items)
+		{
+			for (int i = 0; i < items.Count; i++)
+			{
+				if (data.Count == 0)
+				{
+					data.Add(items[i]);
+					continue;
+				}
+				else
+				{
+					data.Insert(0, items[i]);
+				}
+			}
+		}
+
+		public void Pop()
+		{
+			if (data.Count != 0)
+			{
+				data.RemoveAt(0);
+			}
+			else
+			{
+				Console.WriteLine("No elements");
+				Environment.Exit(0);
+			}
+		}
+
+		public void Print()
+		{
+			foreach (var item in data)
+			{
+				Console.WriteLine(item);
+			}
+			foreach (var item in data)
+			{
+				Console.WriteLine(item);
+			}
+		}
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (int i = 0; i < data.Count; i++)
+			{
+				yield return data[i];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+	}
+}
