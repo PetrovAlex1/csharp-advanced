24e7dae89b1057ac3b771ba62379ac2eab432d9b
AlexDays <AlexDays@students.softuni.bg>
2020-08-18 11:17:29 +0300

    Add "ComparingObjects" project

---
+++ IteratorsAndComparatorsExercise/ComparingObjects/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ComparingObjects
+{
+	public class Person : IComparable<Person>
+	{
+		public Person(string name, int age, string town)
+		{
+			this.Name = name;
+			this.Town = town;
+			this.Age = age;
+		}
+
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+
+		public string Town { get; set; }
+
+		public int CompareTo(Person other)
+		{
+			if (this.Town == other.Town && this.Age == other.Age && this.Name == other.Name)
+			{
+				return 0;
+			}
+
+			return -1;
+		}
+
+	}
+}
--- IteratorsAndComparatorsExercise/ComparingObjects/Program.cs
+++ IteratorsAndComparatorsExercise/ComparingObjects/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ComparingObjects
 {
@@ -6,7 +8,40 @@ namespace ComparingObjects
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var input = Console.ReadLine().Split().ToArray();
+			var people = new List<Person>();
+
+			while (true)
+			{
+				if (input[0].ToLower() == "end")
+				{
+					break;
+				}
+
+				var person = new Person(input[0], int.Parse(input[1]), input[2]);
+				people.Add(person);
+				input = Console.ReadLine().Split().ToArray();
+			}
+
+			var position = int.Parse(Console.ReadLine());
+			var samePerson = 0;
+
+			foreach (var person in people)
+			{
+				if (person.CompareTo(people[position - 1]) == 0)
+				{
+					samePerson++;
+				}
+			}
+
+			if (samePerson == 1)
+			{
+				Console.WriteLine("No matches");
+			}
+			else
+			{
+				Console.WriteLine($"{samePerson} {people.Count - samePerson} {people.Count}");
+			}
 		}
 	}
 }
